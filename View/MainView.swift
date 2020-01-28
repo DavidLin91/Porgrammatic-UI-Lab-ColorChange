5e1a9d5aeb555fba2e92cbfa66cd5ff1144f81d7
David <lin.david985@gmail.com>
2020-01-28 17:12:42 -0500

    Initial Commit

---
+++ View/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Porgrammatic-UI-Lab-ColorChange
+//
+//  Created by David Lin on 1/28/20.
+//  Copyright © 2020 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
